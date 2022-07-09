0baa37f1590d02030717f21ffd5aa7f783b96333
xganss <79078097+S4MUR41i@users.noreply.github.com>
2022-07-09 07:33:30 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 let fs = require('fs')        
 let chalk = require('chalk')
 global.owner = [
-  ['6289509960601'],
-  ['6289509960601'],
-  ['6289509960601', 'Deffri Gans', true]  
+  ['6288227248988'],
+  ['6288227248988'],
+  ['6288227248988', 'Asrori Amin', true]  
   
 ] 
-global.ownername = [['𝐃𝐞𝐟𝐟𝐫𝐢 𝐆𝐚𝐧𝐬']]
-global.namabot = [['𝑮𝒂𝒏𝒔𝑩𝑶𝑻𝒛']]
-global.ownerinsta = [['@b4c00t.dtz']]
-global.instalu = [['instagram.com/b4c00t.dtz']]
-global.ytlu = [['https://youtube.com/channel/UCmEQy5B3GhmfNVSrHEzv-uA']]
+global.ownername = [['Asrori Amin']]
+global.namabot = [['Amin Bot']]
+global.ownerinsta = [['@saya_asroriamin']]
+global.instalu = [['instagram.com/saya_asroriamin']]
+global.ytlu = [['https://youtube.com/channel/UCaybv50unIvy2sNCfqigusw']]
 global.mods = [] 
-global.prems = ['6289509960601'] 
+global.prems = ['6288227248988'] 
 
 global.APIs = { 
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -43,8 +43,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '𝐃𝐞𝐟𝐟𝐫𝐢 𝐆𝐚𝐧𝐬'
-  var sticker_author = '© 𝑮𝒂𝒏𝒔𝑩𝑶𝑻𝒛'
+  var sticker_name = 'Asrori Amin'
+  var sticker_author = ''
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -59,9 +59,9 @@ fs.watchFile(file_exif, () => {
 })
 
 // Sticker WM
-global.packname = '𝐃𝐞𝐟𝐟𝐫𝐢 𝐆𝐚𝐧𝐬'
-global.author = '𝑮𝒂𝒏𝒔𝑩𝑶𝑻𝒛'
-global.wm = '© Deffri Gans'
+global.packname = 'Asrori Amin'
+global.author = ''
+global.wm = ''
 
 global.multiplier = 100
 
